d9cee45443f9050df501eeb4e848447226533d75
imran-lgtm <imran@restcorp.net>
2026-04-02 17:50:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-
 from kivy.app import App
-
 from kivy.uix.boxlayout import BoxLayout
-
 from kivy.uix.label import Label
 
-
-
 class PlanteverythingApp(App):
-
     def build(self):
-
         return Label(text="🌿 Planteverything V2 Ready! 🌿\n(Encyclopedia & AI Chat)")
 
-
-
 if __name__ == '__main__':
-
     PlanteverythingApp().run()
